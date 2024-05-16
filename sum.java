79bd5da673f5ab9898a970663ddf05caa9f27a03
Raj <168058337+boommnti@users.noreply.github.com>
2024-05-16 21:22:48 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,13 @@
+class sum
+{
+void add(int a,int b)
+{
+int c=a+b;
+System.out.println("sum="+c);
+}
+void add(String s1,String s2)
+{
+String s3=s1+s2;
+System.out.println("the joined string is "+s3);
+}
+}
